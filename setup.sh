31aeb8ea0cb61d73bfd989a906a4f9da2b038fb6
perzizzle <michaelperzel@gmail.com>
2023-06-16 09:07:44 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+pip3 install --upgrade pip 
+pip3 install wheel 
+pip3 install ansible 
+yum install python2-devel python2-pip -y 
+pip2 install Flask uwsgi 
+mkdir /var/save 
+chown tasker:tasker /var/save 
+
+exit 0
