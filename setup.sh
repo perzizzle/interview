9199960459ddff8b338e4c0688659f51a448c445
perzizzle <michaelperzel@gmail.com>
2023-07-05 10:44:04 -0500

    Don't fail if directory already exists

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ pip3 install wheel
 pip3 install ansible 
 yum install python2-devel python2-pip -y 
 pip2 install Flask uwsgi 
-mkdir /var/save 
+mkdir -p /var/save 
 chown tasker:tasker /var/save 
 
 exit 0
